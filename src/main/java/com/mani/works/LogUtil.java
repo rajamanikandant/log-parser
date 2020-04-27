1cac63bf6459cb40eb99504e690542af46ede4b8
Rajamanikandan Thiagarajan <rthiagarajan@rthiagaraja-a24.vmware.com>
2020-04-27 21:56:27 +0530

    add filter by procID

--- src/main/java/com/mani/works/LogUtil.java
+++ src/main/java/com/mani/works/LogUtil.java
@@ -1,10 +1,48 @@
 package com.mani.works;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
+
 public class LogUtil {
+    private static final String filepath = "/Users/rthiagarajan/Downloads/temp.log";
+    private static List<String> logList;
+
+
     public static void parseLog(String procID, List<String> keywords) {
+        String logRegex = "([\\d-]+)\\s+([\\d:\\.]+)\\s+([\\d]+)\\s+([\\d]+)\\s+([A-Z])\\s+([\\w]+)\\s+:(.+)";
+        Pattern logPattern = Pattern.compile(logRegex);
+
+
+        filterBy(procID);
+        Stream<String> filteredLogs = logList.stream();
+        filteredLogs.distinct().forEach(System.out::println);
+
+    }
+
+    private static void filterBy(String procID) {
+        final String logRegex = String.format("([\\d-]+)\\s+([\\d:\\.]+)\\s+(%s)\\s+([\\d]+)\\s+([A-Z])\\s+([\\w]+)\\s+:(.+)", procID);
+        final Pattern logPattern = Pattern.compile(logRegex);
+
+        try {
+            Stream<String> logEntries = Files.lines(Paths.get(filepath));
+            logList = logEntries
+                    .filter(logPattern.asPredicate())
+//                    .filter(line -> line.contains(procID))
+                    .collect(toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String trimTimeStamp(String input) {
+        return "";
 
     }
 
@@ -21,4 +59,5 @@ public class LogUtil {
     private static Stream keywordsFinder(Stream input, List<String> keywords) {
         return null;
     }
+
 }
---
+++ src/main/java/com/mani/works/Test.java
@@ -0,0 +1,7 @@
+package com.mani.works;
+
+public class Test {
+    public static void main(String[] args) {
+        LogUtil.parseLog("26071", null);
+    }
+}
