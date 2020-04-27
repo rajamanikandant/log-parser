d1c47ff493f138b2cc1a9ab55d835c4ff898e6ea
Rajamanikandan Thiagarajan <rthiagarajan@rthiagaraja-a25.vmware.com>
2020-04-28 04:05:50 +0530

    add error print

--- src/main/java/com/mani/works/LogUtil.java
+++ src/main/java/com/mani/works/LogUtil.java
@@ -26,55 +26,9 @@ public class LogUtil {
         List<String> logsByProcID;
         logsByProcID = filterBy(procID);
 
-//        Stream<String> filteredLogs = logsByProcID.stream();
-//        filteredLogs.forEach(System.out::println);
-
-        //Filter by Fatal Exception
-
-        List<String> logs2;
-        logs2 = filterBy(logsByProcID, "Fatal Exception");
-        //Filter by Fatal Exception - Unique exception & count
-
-//        System.out.println("------------------");
-//        printFatalExceptions(logsByProcID);
-//        System.out.println("------------------");
-
-        System.out.println("------------------");
+        printFatalExceptions(logsByProcID);
         printErrors(logsByProcID);
-        System.out.println("------------------");
-//
-//        Map<String, Long> fatalExceptions = logs2.stream()
-//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-//        System.out.println("#####");
-//        System.out.println("FATAL EXCEPTION(S)");
-//        System.out.println("------------------");
-//        fatalExceptions.forEach((k,v) -> System.out.println(k + " | " + v));
-//        System.out.println("#####");
-            //Filter by Fatal Exception - Unique exception & stacktrace
-        //Filter by Errors
-            //Filter by Errors - Unique Errors & count
-        //Filter by Given List of Keywords
-            //Filter by Given List of Keywords - lines & count
-
-
-
-
-
-
-//        logList = filterByNew(procID, keywords.get(0));
-
-        /*
-        Map<String, Long> result = logsByProcID.stream()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-        System.out.println("#----------#");
-        String exceptionStr = logsByProcID.stream().findFirst().get();
-        System.out.println(exceptionStr);
-        System.out.println(logsByProcID.indexOf(exceptionStr));
-        System.out.println("#----------#");
-        result.forEach((k,v) -> System.out.println(k + " | " + v));
-        */
-
+        printByKeywords(logsByProcID, keywords);
     }
 
     private static List<String> filterBy(String procID) {
@@ -87,13 +41,21 @@ public class LogUtil {
             Stream<String> logEntries = Files.lines(Paths.get(classLoader.getResource(filepath).getFile()));
             filteredLines = logEntries
                     .filter(logPattern.asPredicate())
-//                    .map(line -> testTrim(line))
                     .collect(toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
         return filteredLines;
 
+    }
+
+
+    private static List<String> filterBy(List<String> log, String keyword) {
+        List<String> filteredLines = log.stream()
+                .filter(line -> line.toUpperCase().contains(keyword.toUpperCase()))
+                .map(line -> trimLogLine(line))
+                .collect(toList());
+        return filteredLines;
     }
 
 
@@ -139,7 +101,6 @@ public class LogUtil {
             }
         }
 
-        System.out.println("#####");
         System.out.println("FATAL EXCEPTION(S)");
         System.out.println("------------------");
         System.out.println("Exception Message| # of Occurrences");
@@ -150,16 +111,16 @@ public class LogUtil {
         System.out.println("Stacktrace:");
         System.out.println("-----------");
         System.out.println(stackTraces.toString());
-        System.out.println("#####");
+        System.out.println("-------------------------------------------------------");
+        System.out.println();
+        System.out.println();
 
     }
-
 
 
     private static void printErrors(List<String> log) {
         final String logRegex = "[\\d-]+\\s+[\\d:\\.]+\\s+[\\d]+\\s+[\\d]+\\s+E\\s+[\\w]+\\s*:(?!\\s+at ).+";
         final Pattern logPattern = Pattern.compile(logRegex);
-        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 
         List<String> filteredLines = log.stream()
                 .filter(logPattern.asPredicate())
@@ -168,7 +129,6 @@ public class LogUtil {
                 .collect(toList());
 
 
-        System.out.println("#####");
         System.out.println("Error(s)");
         System.out.println("--------");
         System.out.println("Error Message| # of Occurrences");
@@ -176,113 +136,51 @@ public class LogUtil {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         fatalExceptions.forEach((k,v) -> System.out.println(k + " | " + v));
 
+
+        System.out.println("-------------------------------------------------------");
         System.out.println();
         System.out.println();
 
-        System.out.println("#####");
-
-    }
-
-    private static List<String> filterBy(List<String> log, String keyword) {
-        List<String> filteredLines = log.stream()
-                .filter(line -> line.toUpperCase().contains(keyword.toUpperCase()))
-                .map(line -> trimLogLine(line))
-                .collect(toList());
-        return filteredLines;
     }
 
 
-    private static List<String> filterByNew(String procID, String keyword) {
-        final String logRegex = String.format("[\\d-]+\\s+[\\d:\\.]+\\s+%s\\s+.+", procID);
-        final Pattern logPattern = Pattern.compile(logRegex);
-        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        List<String> filteredLines = null;
+    private static void printByKeywords(List<String> log, List<String> keywords) {
 
-        try {
-            Stream<String> logEntries = Files.lines(Paths.get(classLoader.getResource(filepath).getFile()));
-            filteredLines = logEntries
-                    .filter(logPattern.asPredicate())
-                    .filter(line -> line.contains(keyword))
-//                    .map(line -> testTrim(line))
-                    .collect(toList());
-        } catch (IOException e) {
-            e.printStackTrace();
+        System.out.println("Matching String(s)");
+        System.out.println("------------------");
+        System.out.println("Matching String | # of Occurrences");
+
+        for(String s:keywords) {
+            printByKeyword(log, s);
         }
-        return filteredLines;
+
+        System.out.println("-------------------------------------------------------");
+        System.out.println();
+        System.out.println();
 
     }
-
-
-
-    private static void printStackTrace(List<String> logEntries, String procID) {
-        String keyword = "NoSuchMethodException";
-
-//        logEntries.indexOf()
-
-
-    }
-
-
-    private static List<String> filterBy(String procID, String keyword) {
-        final String logRegex = String.format("[\\d-]+\\s+[\\d:\\.]+\\s+%s\\s+[\\d]+\\s+([A-Z]\\s+[\\w]+\\s+:.+%s.+)", procID, keyword);
+    private static void printByKeyword(List<String> log, String keyword) {
+        final String logRegex = String.format("[\\d-]+\\s+[\\d:\\.]+\\s+[\\d]+\\s+[\\d]+\\s+[A-Z]+\\s+[\\w]+\\s*:\\s+.*%s.*", keyword);
         final Pattern logPattern = Pattern.compile(logRegex);
-        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         List<String> filteredLines = null;
+        filteredLines = log.stream()
+            .filter(logPattern.asPredicate())
+            .map(line -> trimLogLine(line))
+            .collect(toList());
 
-        try {
-            Stream<String> logEntries = Files.lines(Paths.get(classLoader.getResource(filepath).getFile()));
-            filteredLines = logEntries
-                    .filter(logPattern.asPredicate())
-                    .map(line -> trimLogLine(line))
-                    .collect(toList());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return filteredLines;
+        Map<String, Long> fatalExceptions = filteredLines.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        fatalExceptions.forEach((k,v) -> System.out.println(k + " | " + v));
 
     }
-
 
     private static String trimLogLine(String logEntry) {
 
-        final String logRegex = "[\\d-]+\\s+[\\d:\\.]+\\s+[\\d]+\\s+[\\d]+\\s+[A-Z]\\s+[\\w]+:\\s+(.+)";
+        final String logRegex = "[\\d-]+\\s+[\\d:\\.]+\\s+[\\d]+\\s+[\\d]+\\s+[A-Z]\\s+[\\w]+\\s*:\\s+(.+)";
         final Pattern logPattern = Pattern.compile(logRegex);
         Matcher matcher = logPattern.matcher(logEntry);
 
         return matcher.matches() ? matcher.group(1) : "";
-    }
-
-    private static void loadLogFile() {
-        String fileName = "android.log";
-
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-
-        File file = new File(classLoader.getResource(fileName).getFile());
-
-        try {
-            Stream<String> temp = Files.lines(Paths.get(classLoader.getResource(fileName).getFile()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String trimTimeStamp(String input) {
-        return "";
-
-    }
-
-    private static Stream listUniqueExceptions(Stream input) {
-        return null;
-    }
-
-
-    private static Stream printStackTrace(Stream input) {
-        return null;
-    }
-
-
-    private static Stream keywordsFinder(Stream input, List<String> keywords) {
-        return null;
     }
 
 }
--- src/main/java/com/mani/works/Test.java
+++ src/main/java/com/mani/works/Test.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        LogUtil.parseLog("4667", Arrays.asList("Exception"));
+        LogUtil.parseLog("4667", Arrays.asList("OutOfMemoryError"));
 //        String op = LogUtil.testTrim("08-27 18:30:57.806  4914  4914 E subsystem_ramdump: Unable to create /sdcard/ramdump");
 //        System.out.println(op);
     }
