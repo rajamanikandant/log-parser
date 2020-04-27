cc2dc87957425682774472461d253478f4436bcf
Rajamanikandan Thiagarajan <rthiagarajan@rthiagaraja-a24.vmware.com>
2020-04-27 11:53:50 +0530

    init

---
+++ src/main/java/com/mani/works/LogUtil.java
@@ -0,0 +1,24 @@
+package com.mani.works;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LogUtil {
+    public static void parseLog(String procID, List<String> keywords) {
+
+    }
+
+    private static Stream listUniqueExceptions(Stream input) {
+        return null;
+    }
+
+
+    private static Stream printStackTrace(Stream input) {
+        return null;
+    }
+
+
+    private static Stream keywordsFinder(Stream input, List<String> keywords) {
+        return null;
+    }
+}
